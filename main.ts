dd18e749cc0d7ad58772547b92452d859c45ab8d
awsumcat <98213301+awsumcat@users.noreply.github.com>
2022-06-03 17:45:25 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
-})
 input.onButtonPressed(Button.A, function () {
     player.change(LedSpriteProperty.Y, 1)
 })
@@ -25,10 +22,28 @@ player.set(LedSpriteProperty.Blink, 500)
 lives = 3
 points = 0
 let speed = 1000
-radio.setGroup(1)
 basic.forever(function () {
     basic.pause(speed)
     enemy.change(LedSpriteProperty.X, 1)
+})
+basic.forever(function () {
+    if (enemy.get(LedSpriteProperty.X) == 4) {
+        basic.pause(speed)
+        points += 1
+        enemy.set(LedSpriteProperty.X, 0)
+        enemy.set(LedSpriteProperty.Y, randint(0, 4))
+        if (points % 10 == 0 && points != 0) {
+            speed = speed / 2
+            music.playTone(392, music.beat(BeatFraction.Eighth))
+        }
+    }
+})
+basic.forever(function () {
+    if (speed == 125) {
+        game.gameOver()
+        basic.clearScreen()
+        basic.showString("game won!!!")
+    }
 })
 basic.forever(function () {
     if (enemy.isTouching(player)) {
@@ -39,29 +54,7 @@ basic.forever(function () {
         if (lives == 0) {
             music.playMelody("C5 A C5 B F F A B ", 200)
             basic.clearScreen()
-            basic.showString("you lost!!!!!")
+            game.gameOver()
         }
-    }
-})
-basic.forever(function () {
-    if (enemy.get(LedSpriteProperty.X) == 4) {
-        basic.pause(speed)
-        points += 1
-        enemy.set(LedSpriteProperty.X, 0)
-        enemy.set(LedSpriteProperty.Y, randint(0, 4))
-    }
-})
-basic.forever(function () {
-    if (speed == 100) {
-        basic.clearScreen()
-        basic.showString("game won!!!")
-        basic.showNumber(points)
-    }
-})
-basic.forever(function () {
-    if (points % 10 == 0 && points != 0) {
-        speed = speed / 2
-        music.playTone(392, music.beat(BeatFraction.Eighth))
-        radio.sendNumber(speed)
     }
 })
